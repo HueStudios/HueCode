159b0f4432745dd913a156bc4581e9ed09935330
Unknown <ozjuanpa@gmail.com>
2017-11-27 17:57:36 -0500

    Node design

---
+++ HueCode/Assets/Draggable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	Vector2 Delta;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnBeginDrag(PointerEventData eventData) {
+		Delta = this.transform.position - (Vector3)eventData.position;
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		this.transform.position = eventData.position + Delta;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+	}
+}
