607b0c1e2842752f2008880ddfb400b520fd483f
Unknown <ozjuanpa@gmail.com>
2017-11-29 13:30:52 -0500

    Context menu spawns on right click

---
+++ HueCode/Assets/Frontend/ContextMenuCreator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextMenuCreator : MonoBehaviour {
+	public GameObject menu;
+	public GameObject mainMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			Vector2 menuPosition = Input.mousePosition;
+			Vector2 fixedPosition = menuPosition + new Vector2 (menu.GetComponent<RectTransform> ().sizeDelta.x / 2, -menu.GetComponent<RectTransform> ().sizeDelta.y / 2);
+			Debug.Log(new Vector2 (menu.GetComponent<RectTransform> ().sizeDelta.x / 2, menu.GetComponent<RectTransform> ().sizeDelta.x / 2));
+			Destroy (mainMenu);
+			mainMenu = CreateMenu (fixedPosition);
+		}
+	}
+
+	public GameObject CreateMenu (Vector2 position)
+	{
+		GameObject menuInstance = GameObject.Instantiate (menu);
+		menuInstance.transform.SetParent (GameObject.Find("Canvas").transform);
+		menuInstance.GetComponent<RectTransform> ().anchoredPosition = position;
+		return menuInstance;
+	}
+}
