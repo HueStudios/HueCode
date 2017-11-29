e232a5b5d4a8934a7c29d43ee331cb1e80bf3fb7
Unknown <ozjuanpa@gmail.com>
2017-11-29 14:25:50 -0500

    Separated the MenuCreator class into two different classes

--- HueCode/Assets/Frontend/ContextMenuCreator.cs
+++ HueCode/Assets/Frontend/ContextMenuCreator.cs
@@ -4,29 +4,6 @@ using UnityEngine;
 
 public class ContextMenuCreator : MonoBehaviour {
 	public GameObject menu;
-	public GameObject mainMenu;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (1)) {
-			Vector2 menuPosition = Input.mousePosition;
-			Vector2 fixedPosition = menuPosition + new Vector2 (menu.GetComponent<RectTransform> ().sizeDelta.x / 2, -menu.GetComponent<RectTransform> ().sizeDelta.y / 2);
-			Destroy (mainMenu);
-			mainMenu = CreateMenu (fixedPosition);
-			mainMenu.GetComponent<ContextMenu> ().AddElement ("Create new node...", NewNode);
-		}
-	}
-
-	void NewNode (ContextMenu menu)
-	{
-		Debug.Log (":0");
-		menu.CloseMenu ();
-	}
-
 	public GameObject CreateMenu (Vector2 position)
 	{
 		GameObject menuInstance = GameObject.Instantiate (menu);
---
+++ HueCode/Assets/Frontend/DefaultContextMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultContextMenu : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public ContextMenuCreator creator;
+	// Use this for initialization
+	void Start () {
+		creator = GetComponent<ContextMenuCreator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			Vector2 menuPosition = Input.mousePosition;
+			Vector2 fixedPosition = menuPosition + new Vector2 (creator.menu.GetComponent<RectTransform> ().sizeDelta.x / 2, -creator.menu.GetComponent<RectTransform> ().sizeDelta.y / 2);
+			Destroy (mainMenu);
+			mainMenu = creator.CreateMenu (fixedPosition);
+			mainMenu.GetComponent<ContextMenu> ().AddElement ("Create new node...", NewNode);
+		}
+	}
+
+	void NewNode (ContextMenu menu)
+	{
+		Debug.Log (":0");
+		menu.CloseMenu ();
+	}
+
+}
