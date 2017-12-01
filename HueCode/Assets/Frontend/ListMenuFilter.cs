7545fa69ecb7a5c07f134b96a012dbc400d768d8
Unknown <ozjuanpa@gmail.com>
2017-12-01 16:25:33 -0500

    ListMenu filter

---
+++ HueCode/Assets/Frontend/ListMenuFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListMenuFilter : MonoBehaviour {
+	public ListMenu toFilter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Filter()
+	{
+		string filterText = GetComponent<InputField> ().text;
+		if (toFilter) {
+			if (filterText == "") {
+				foreach (GameObject e in toFilter.elements) {
+					e.SetActive (true);
+				}
+			} else {
+				foreach (GameObject e in toFilter.elements) {
+					if (e.transform.Find ("Text").GetComponent<Text> ().text.ToLower ().Contains (filterText.ToLower ())) {
+						e.SetActive (true);
+					} else {
+						e.SetActive (false);
+					}
+				}
+			}
+		}
+	}
+}
