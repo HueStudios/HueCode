1005137daea89663cd441065ca2cf15d0b40869f
1Macho <ozjuanpa@gmail.com>
2017-12-07 18:21:46 -0500

    Argument option adder

---
+++ HueCode/Assets/Frontend/ArgumentOptionAdder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArgumentOptionAdder : MonoBehaviour {
+
+	public GameObject argumentOption;
+	public GameObject optionAdder;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddOption()
+	{
+		Transform parent = transform.parent;
+		GameObject newOption = Instantiate(argumentOption);
+		newOption.transform.SetParent(parent);
+		GameObject newAdder = Instantiate(optionAdder);
+		newAdder.transform.SetParent(parent);
+		Destroy(this.gameObject);
+	}
+}
